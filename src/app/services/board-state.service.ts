81a071ec446283b3096a3a7fec32fed382cdf931
William RAD Funk <contact@WILLIAMRADFunk.com>
2019-11-22 12:31:37 -0900

    Puts little temporary crowns on frogs when they reach other end. Uses a simpler helper function to set up the board. Makes correction to king chain jumping algorithm.

--- src/app/services/board-state.service.ts
+++ src/app/services/board-state.service.ts
@@ -8,410 +8,7 @@ import { Cell } from '../models/cell';
 })
 export class BoardStateService {
 	private _activePlayer: BehaviorSubject<number> = new BehaviorSubject<number>(1);
-	private readonly _boardState: BehaviorSubject<Board> = new BehaviorSubject<Board>({
-		cellStates: [
-			[
-				{
-					player: 0,
-					playerColor: '',
-					position: [0, 0],
-					value: 0
-				},
-				{
-					player: 2,
-					playerColor: 'black',
-					position: [0, 1],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [0, 2],
-					value: 0
-				},
-				{
-					player: 2,
-					playerColor: 'black',
-					position: [0, 3],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [0, 4],
-					value: 0
-				},
-				{
-					player: 2,
-					playerColor: 'black',
-					position: [0, 5],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [0, 6],
-					value: 0
-				},
-				{
-					player: 2,
-					playerColor: 'black',
-					position: [0, 7],
-					value: 1
-				},
-			],
-			[
-				{
-					player: 2,
-					playerColor: 'black',
-					position: [1, 0],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [1, 1],
-					value: 0
-				},
-				{
-					player: 2,
-					playerColor: 'black',
-					position: [1, 2],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [1, 3],
-					value: 0
-				},
-				{
-					player: 2,
-					playerColor: 'black',
-					position: [1, 4],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [1, 5],
-					value: 0
-				},
-				{
-					player: 2,
-					playerColor: 'black',
-					position: [1, 6],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [1, 7],
-					value: 0
-				},
-			],
-			[
-				{
-					player: 0,
-					playerColor: '',
-					position: [2, 0],
-					value: 0
-				},
-				{
-					player: 2,
-					playerColor: 'black',
-					position: [2, 1],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [2, 2],
-					value: 0
-				},
-				{
-					player: 2,
-					playerColor: 'black',
-					position: [2, 3],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [2, 4],
-					value: 0
-				},
-				{
-					player: 2,
-					playerColor: 'black',
-					position: [2, 5],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [2, 6],
-					value: 0
-				},
-				{
-					player: 2,
-					playerColor: 'black',
-					position: [2, 7],
-					value: 1
-				},
-			],
-			[
-				{
-					player: 0,
-					playerColor: '',
-					position: [3, 0],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [3, 1],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [3, 2],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [3, 3],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [3, 4],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [3, 5],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [3, 6],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [3, 7],
-					value: 0
-				},
-			],
-			[
-				{
-					player: 0,
-					playerColor: '',
-					position: [4, 0],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [4, 1],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [4, 2],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [4, 3],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [4, 4],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [4, 5],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [4, 6],
-					value: 0
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [4, 7],
-					value: 0
-				},
-			],
-			[
-				{
-					player: 1,
-					playerColor: 'white',
-					position: [5, 0],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [5, 1],
-					value: 0
-				},
-				{
-					player: 1,
-					playerColor: 'white',
-					position: [5, 2],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [5, 3],
-					value: 0
-				},
-				{
-					player: 1,
-					playerColor: 'white',
-					position: [5, 4],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [5, 5],
-					value: 0
-				},
-				{
-					player: 1,
-					playerColor: 'white',
-					position: [5, 6],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [5, 7],
-					value: 0
-				},
-			],
-			[
-				{
-					player: 0,
-					playerColor: '',
-					position: [6, 0],
-					value: 0
-				},
-				{
-					player: 1,
-					playerColor: 'white',
-					position: [6, 1],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [6, 2],
-					value: 0
-				},
-				{
-					player: 1,
-					playerColor: 'white',
-					position: [6, 3],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [6, 4],
-					value: 0
-				},
-				{
-					player: 1,
-					playerColor: 'white',
-					position: [6, 5],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [6, 6],
-					value: 0
-				},
-				{
-					player: 1,
-					playerColor: 'white',
-					position: [6, 7],
-					value: 1
-				},
-			],
-			[
-				{
-					player: 1,
-					playerColor: 'white',
-					position: [7, 0],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [7, 1],
-					value: 0
-				},
-				{
-					player: 1,
-					playerColor: 'white',
-					position: [7, 2],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [7, 3],
-					value: 0
-				},
-				{
-					player: 1,
-					playerColor: 'white',
-					position: [7, 4],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [7, 5],
-					value: 0
-				},
-				{
-					player: 1,
-					playerColor: 'white',
-					position: [7, 6],
-					value: 1
-				},
-				{
-					player: 0,
-					playerColor: '',
-					position: [7, 7],
-					value: 0
-				},
-			],
-		]
-	});
+	private readonly _boardState: BehaviorSubject<Board> = new BehaviorSubject<Board>(this._resetBoard());
 	private readonly _clickableCellIds: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
 	private readonly _readyToSubmit: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 	private readonly _moveChainIds: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
@@ -431,8 +28,8 @@ export class BoardStateService {
 		this._moveChainCells.length = 0;
 		this._moveChainIds.next([]);
 
-		this._activePlayer.next(this._activePlayer.value === 1 ? 2 : 1);
 		this._crownKings();
+		this._activePlayer.next(this._activePlayer.value === 1 ? 2 : 1);
 
 		this._clickableCellIds.next(this._findClickableCells(this._activePlayer.value));
 	}
@@ -526,7 +123,7 @@ export class BoardStateService {
 			return ids;
 		}
 		// If a king, combine upward and downward, otherwise restrict to direction of player.
-		if (this._moveChainCells[this._moveChainCells.length - 1].value === 2) {
+		if (this._moveChainCells[0].value === 2) {
 			return [...this._upwardPathValidOptions(this._moveChainCells[this._moveChainCells.length - 1], board), ...this._downwardPathValidOptions(this._moveChainCells[this._moveChainCells.length - 1], board)];
 		} else {
 			return direction === 1 ? this._upwardPathValidOptions(this._moveChainCells[this._moveChainCells.length - 1], board) : this._downwardPathValidOptions(this._moveChainCells[this._moveChainCells.length - 1], board);
@@ -564,6 +161,32 @@ export class BoardStateService {
 		cellStateBefore.player = 0;
 		cellStateBefore.value = 0;
 		cellStateBefore.playerColor = '';
+	}
+
+	_resetBoard() {
+		const board = { cellStates: [] };
+		for (let row = 0; row < 8; row++) {
+			board.cellStates[row] = [];
+			for (let col = 0; col < 8; col++) {
+				const cell = {
+					player: 0,
+					playerColor: '',
+					position: [row, col],
+					value: (row % 2) + (col % 2) === 1 ? 1 : 0
+				};
+				if (row < 3) {
+					cell.player = cell.value ? 2 : 0;
+					cell.playerColor = cell.value ? 'black' : '';
+				} else if (row > 4) {
+					cell.player = cell.value;
+					cell.playerColor = cell.value ? 'white' : '';
+				} else {
+					cell.value = 0;
+				}
+				board.cellStates[row][col] = cell;
+			}
+		}
+		return board;
 	}
 
 	_upperPathValidCheck(cell: Cell, board: Cell[][]): boolean {
